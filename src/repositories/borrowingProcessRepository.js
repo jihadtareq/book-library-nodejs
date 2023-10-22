1f258568b142b0adfa16f2fc82bd2e937fec2906
JihadTareq <jihad@dooolab.com>
2023-10-22 11:34:15 +0300

    updates

--- src/repositories/borrowingProcessRepository.js
+++ src/repositories/borrowingProcessRepository.js
@@ -1,10 +1,12 @@
 const Repository = require('./baseRepositry')
-const date = require('date-and-time') 
-const now  =  new Date(); 
-
+const { QueryTypes } = require('sequelize');
 const BorrowingProcess = require('../database/models').borrowingProcess
 const Book = require('../database/models').book
 const Borrower = require('../database/models').borrower
+const sequelize = require('../database/models').sequelize
+
+const date = require('date-and-time') 
+const now  =  new Date(); 
 
 class BorrowingProcessRepository extends Repository {
   constructor () {
@@ -45,6 +47,14 @@ class BorrowingProcessRepository extends Repository {
 
   async returnBook(id){
     return await this.model.update({returnedDate:date.format(now,'YYYY-MM-DD HH:mm:ss')}, {where: { id } })
+  }
+
+    //CUSTOME SEARCH
+  async searchQuery (customeQuery) { 
+    return await sequelize.query(`SELECT * FROM borrowing_processes WHERE ${customeQuery}`, { type: QueryTypes.SELECT });
+  }
+  async getProcessesNotOverDueDateYet () { 
+    return await sequelize.query('SELECT * FROM borrowing_processes WHERE is_over_due = false', { type: QueryTypes.SELECT });
   }
 }
 
